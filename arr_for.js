0c2f0b394342077f700254d69b5f5f6e8c6ff8d6
wjdwkdgns9658 <jjh844357@gmail.com>
2024-05-29 12:56:31 +0900

    let

--- arr_for.js
+++ arr_for.js
@@ -1,16 +1,57 @@
-const nav_data = [
-    {
-        gnb_li_a : "회사소개",
-        gnb_li_href : "./comapny.html"
-    },
-    {
-        gnb_li_a : "제품소개",
-        gnb_li_href : "./product.html"
-    },
-    {
-        gnb_li_a : "커뮤니티",
-        gnb_li_href : "./board.html"
-    }
-]
+// const nav_data = [
+//     {
+//         gnb_li_a : "회사소개",
+//         gnb_li_href : "./comapny.html"
+//     },
+//     {
+//         gnb_li_a : "제품소개",
+//         gnb_li_href : "./product.html"
+//     },
+//     {
+//         gnb_li_a : "커뮤니티",
+//         gnb_li_href : "./board.html"
+//     }
+// ]
 
 // console.log(nav_data[2].gnb_li_a)
+
+console.log("한번만 실행");
+
+
+for(var i = 0; i < 5; i++ ){
+console.log("난 다섯번 실행");
+}
+
+// let count = 10;
+// count++; //후치연산 count += 1 /++count(전치연산)
+// count++;
+// count++;
+// count++;
+
+// let count = 10;
+//     count +=100; 
+//     count +=1; //111
+//     ++count;  //112
+//     count++;  //112
+
+// console. log(count); //113
+
+// let count = 10;
+//     count +=100; 
+//     count =1; //111
+//     ++count;  //112
+//     count++;
+
+let multy = 10;
+    multy = multy * 3; // multy *= 3; 곱하기(*)
+    multy = multy / 3; // multy /= 3; 나누기(/)
+
+//문자연산식
+let mystudy = "리액트"
+    mystudy = "리액트" + "뷰" ; //mystudy += "뷰" ;
+
+let startjs = 10; 
+    startjs += 10;   //20
+    startjs += "원"; //text가 들어가는 순간 문장이 된다
+    startjs += 100; //10원100
+    startjs += 100; //10원100100
